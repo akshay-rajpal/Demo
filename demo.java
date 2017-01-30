c680313d4dcd9a80d58250b6a103f6f4ea3648e7
Akshay Rajpal <akshay.rajpal@intimetec.com>
2017-01-30 09:29:42 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -3,7 +3,7 @@ class Demo{
    public static void main(String[] args){
 
      System.out.println("Akshay Rajpal!!");
-    System.out.println("hello!!!");
+    System.out.println("hello!");
             
    }
 
